75c542086e8256092a3934629659cb9afb6cf5a5
Shaikh Suzain <shaikhsuzain111@gmail.com>
2025-06-30 02:26:38 +0530

    Day 6: String basics, methods, palindrome check

---
+++ day6_string_methods.py
@@ -0,0 +1,16 @@
+name = "Shaikh Suzain"
+
+# Capitalize, Upper, Lower
+print("Capitalized:",name.capitalize())
+print("Upper",name.upper())
+print("Lower:", name.lower())
+
+
+# Count, Find, Replace
+print("Count of 'a':", name.count('a'))
+print("Position of 'b':", name.find('b'))
+print(name.replace('Shaikh','Agent'))
+
+# Strip (remove whitespace)
+line="Hello World!      "
+print("Stripped:",line.strip())
---
+++ day6_strings_basics.py
@@ -0,0 +1,11 @@
+message= "Hello, Everyone!"
+print("Message: ",message)
+
+print("First char:",message[0])
+print("Last char:",message[-1])
+
+print("First 5 letter:",message[:5])
+print("Middle Section:",message[7:13])
+
+for char in message:
+    print(char)
